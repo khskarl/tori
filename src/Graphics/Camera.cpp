90a5a084f4567b3e966da84d4a81e19a3a596365
Eu <karllhenning64@gmail.com>
2017-08-12 00:20:42 -0300

    Rewrote camera class to use Pitch and Yaw

--- src/Graphics/Camera.cpp
+++ src/Graphics/Camera.cpp
@@ -4,29 +4,27 @@
 #include <glm/gtc/matrix_transform.hpp>
 
 Camera::Camera () {
-	mAspectRatio = mWindowWidth / mWindowHeight;
 	this->BuildProjectionMatrix();
 }
 
 Camera::Camera (const glm::vec3 pos, const glm::vec3 direction, const float width, const float height) {
-	mPosition = pos;
-	mDirection = direction;
-	mWindowWidth = width;
-	mWindowHeight = height;
-	mAspectRatio = width / height;
+	m_position  = pos;
+	m_direction = direction;
+	m_width  = width;
+	m_height = height;
 
 	this->BuildProjectionMatrix();
 }
 
 void Camera::BuildProjectionMatrix () {
-	mProjectionMatrix = glm::perspective(glm::radians(mFov),
-	                                     mAspectRatio,
-	                                     mNearPlane,
-	                                     mFarPlane);
+	mProjectionMatrix = glm::perspective(glm::radians(m_fov),
+	                                     GetAspectRatio(),
+	                                     m_nearPlane,
+	                                     m_farPlane);
 }
 
 glm::mat4 const Camera::GetViewMatrix () {
-	return glm::lookAt(mPosition, mPosition + mDirection, mUp);
+	return glm::lookAt(m_position, m_position + m_direction, m_up);
 }
 
 glm::mat4 const& Camera::GetProjectionMatrix () {
@@ -34,35 +32,52 @@ glm::mat4 const& Camera::GetProjectionMatrix () {
 }
 
 glm::vec3 const& Camera::GetPosition () {
-	return mPosition;
+	return m_position;
 }
 
 glm::vec3 const& Camera::GetDirection () {
-	return mDirection;
+	return m_direction;
 }
 
 glm::vec3 const& Camera::GetUp () {
-	return mUp;
+	return m_up;
 }
 
 glm::vec3 const Camera::GetRight () {
-	return glm::normalize(glm::cross(mDirection, mUp));
+	return glm::normalize(glm::cross(m_direction, m_up));
+}
+
+float const Camera::GetAspectRatio () {
+	return m_width / m_height;
 }
 
 void Camera::MoveForward (const float amount) {
-	mPosition += mDirection * amount;
+	m_position += m_direction * amount;
 }
 
 void Camera::MoveRight (const float amount) {
-	glm::vec3 right = GetRight();
-	mPosition += right * amount;
+	m_position += GetRight() * amount;
 }
 
 void Camera::RotatePitch (const float amount) {
-	glm::vec3 right = GetRight();
-	mDirection = glm::normalize(glm::rotate(mDirection, amount, right));
+	m_pitch += amount;
+	if (m_pitch >  90.f) m_pitch =  90.f - 0.001f;
+	if (m_pitch < -90.f) m_pitch = -90.f + 0.001f;
+	RecomputeDirection();
 }
 
 void Camera::RotateYaw (const float amount) {
-	mDirection = glm::normalize(glm::rotate(mDirection, amount, mUp));
+	m_yaw += amount;
+	if (m_yaw > 360.0f) m_yaw = 0.0f;
+	if (m_yaw < 0.0f)   m_yaw = 360.0f;
+	RecomputeDirection();
+}
+
+void Camera::RecomputeDirection () {
+	float pitch = glm::radians(m_pitch);
+	float yaw   = glm::radians(m_yaw);
+	m_direction = (glm::vec3(glm::cos(pitch) * glm::cos(yaw),
+	                         glm::sin(pitch),
+	                         glm::cos(pitch) * glm::sin(yaw)));
+	m_direction = glm::normalize(m_direction);
 }
--- src/Graphics/Camera.hpp
+++ src/Graphics/Camera.hpp
@@ -9,37 +9,41 @@ public:
 	Camera();
 	Camera(const glm::vec3 pos, const glm::vec3 direction, const float width, const float height);
 
-	glm::mat4 const  GetViewMatrix();
-	glm::mat4 const& GetProjectionMatrix();
+	glm::mat4 const  GetViewMatrix ();
+	glm::mat4 const& GetProjectionMatrix ();
 
-	glm::vec3 const& GetPosition();
-	glm::vec3 const& GetDirection();
-	glm::vec3 const& GetUp();
-	glm::vec3 const  GetRight();
+	glm::vec3 const& GetPosition ();
+	glm::vec3 const& GetDirection ();
+	glm::vec3 const& GetUp ();
+	glm::vec3 const  GetRight ();
+	float const GetAspectRatio ();
 
-	float const& GetFov();
+	float const& GetFov ();
 
-	void SetFov(float const& fov);
+	void SetFov (float const& fov);
 
 	void MoveForward (const float amount);
 	void MoveRight   (const float amount);
 	void RotatePitch (const float amount);
 	void RotateYaw   (const float amount);
 private:
-	void BuildProjectionMatrix();
+	void BuildProjectionMatrix ();
+	void RecomputeDirection ();
 
-	glm::vec3 mPosition  = glm::vec3(0);
-	glm::vec3 mDirection = glm::vec3(0, 0, 1);
-	glm::vec3 mUp        = glm::vec3(0, 1, 0);
+	glm::vec3 m_position  = glm::vec3(0);
+	glm::vec3 m_direction = glm::vec3(0, 0, 1);
+	glm::vec3 m_up        = glm::vec3(0, 1, 0);
 
 	glm::mat4 mProjectionMatrix = glm::mat4(1.0f);
 
-	float mFov = 80.0f;
-	float mAspectRatio;
-	float mWindowWidth  = 800.f;
-	float mWindowHeight = 600.f;
-	float mNearPlane = 0.1f;
-	float mFarPlane = 100.f;
+	float m_yaw   = 0.0f;
+	float m_pitch = 0.0f;
+
+	float m_fov = 80.0f;
+	float m_width  = 800.f;
+	float m_height = 600.f;
+	float m_nearPlane = 0.1f;
+	float m_farPlane = 100.f;
 };
 
 #endif
